0a998a352a584333cc8ec1e4964ecacd2cdbc3e2
Demanick05 <66432505+Demanick05@users.noreply.github.com>
2020-11-30 20:55:02 +0200

    Update admin.py

--- Practice/Practice_3/admin.py
+++ Practice/Practice_3/admin.py
@@ -1,42 +1,28 @@
+import uuid
 from .user import User
-from .order import Order
-from .review import Review
-from . import logs_config as record
 
 
-class Customer(User):
-    def __init__(self, username, userpass, first_name, last_name, phone,
-                email, date_of_birth):
+class Administrator(User):
+    def __init__(self, username, userpass, email):
         super().__init__(username, userpass, email)
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone = phone
-        self.date_of_birth = date_of_birth
-        self.bonus_amount = 0
+        self.supply = list()
         self.orders = list()
 
-    def __str__(self):
-        return f"Customer {self.id}: {self.username} ({self.first_name} {self.last_name})"
+    def update_supply(self, suppliers_list):
+        self.supply.clear()
+        for supplier in suppliers_list:
+            self.supply.extend(supplier.supply)
 
-    def create_order(self, item, amount):
-        new_order = Order(self, item, amount)
-        self.orders.append(new_order)
-        return new_order
+    def update_orders(self, customers_list):
+        self.orders.clear()
+        for customer in customers_list:
+            self.orders.extend(customer.orders)
 
-    def approve_review(self):
-        record.logger.debug("Review approved")
-        new_status = "Published"
-        Review.status = new_status
-        return Review.status
-
-
-if __name__ == '__main__':
-    from .item import Item
-    record.logger.debug("Order created")
-    c1 = Customer("iamguido", "4sure", "Guido", "Van Rossum", "000-112-35-8",
-                "guido@python.org", "09-09-1968")
-    i1 = Item("Banana", "Better than ever before", 799.0,
-                ("Golden", "Fresh Green"))
-    c1.create_order(i1, 3)
-    print(c1)
-    print(c1.orders)
+    def check_order(self, order):
+        print(f"Checking order {order.id}")
+        if not order.status == 'New':
+            return order
+        for supply in self.supply:
+            if supply.item == order.item and supply.amount >= order.amount:
+                order.status = 'Confirmed'
+                return order
